3aee8a3d1ef300012641c24c876aa72591c32005
autf <50799173+autf@users.noreply.github.com>
2025-05-12 12:18:33 +0000

    c2s3 (fn vs stmt)

--- MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -44,7 +44,9 @@ example (x : ℝ) : x ≤ x :=
 
 -- Try this.
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  apply lt_trans
+  · apply lt_of_le_of_lt h₀ h₁
+  · apply lt_of_le_of_lt h₂ h₃
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -74,6 +76,7 @@ example (h : 1 ≤ a) (h' : b ≤ c) : 2 + a + exp b ≤ 3 * a + exp c := by
 #check (add_pos : 0 < a → 0 < b → 0 < a + b)
 #check (add_pos_of_pos_of_nonneg : 0 < a → 0 ≤ b → 0 < a + b)
 #check (exp_pos : ∀ a, 0 < exp a)
+#check exp_pos -- Real.exp_pos (x : ℝ) : 0 < rexp x
 #check add_le_add_left
 
 example (h : a ≤ b) : exp a ≤ exp b := by
@@ -86,21 +89,33 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  apply add_le_add_left h₀
 
 example : (0 : ℝ) < 1 := by norm_num
 
+example (h : 0 < c) : 0 < 1 + c := by linarith
+example (h : 0 < exp c) : 0 < 1 + exp c := by linarith
+example : 0 < 1 + exp c := by
+  have h : 0 < exp c := exp_pos c
+  linarith
+
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
+  have h₀ : 0 < 1 + exp a := by linarith [exp_pos a]
+  -- have h₀ : 0 < 1 + exp a := by positivity -- by CGPT
   apply log_le_log h₀
-  sorry
+  apply add_le_add_left
+  apply exp_le_exp.mpr h
 
 example : 0 ≤ a ^ 2 := by
   -- apply?
   exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  apply add_le_add_left
+  linarith [exp_le_exp.mpr h]
 
 example : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
@@ -121,7 +136,17 @@ example : 2*a*b ≤ a^2 + b^2 := by
   linarith
 
 example : |a*b| ≤ (a^2 + b^2)/2 := by
-  sorry
+  apply abs_le'.mpr
+  constructor
+  · have : 0 ≤ a^2 + b^2 - 2*a*b
+    calc
+      0 ≤ (a - b)^2 := by positivity
+      _ = a ^ 2 + b ^ 2 - 2 * a * b := by ring
+    linarith
+  · have : 0 ≤ a^2 + b^2 + 2*a*b
+    calc
+      0 ≤ (a + b)^2 := by positivity
+      _ = a ^ 2 + b ^ 2 + 2 * a * b := by ring
+    linarith
 
 #check abs_le'.mpr
-
