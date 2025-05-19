b22cb9f402f6e1cc6c111d16bfcb5f882cfbd316
autf <autf@dev.null>
2025-05-20 01:10:36 +0800

    c3s5 (max_choice)

--- MIL/C03_Logic/S05_Disjunction.lean
+++ MIL/C03_Logic/S05_Disjunction.lean
@@ -58,19 +58,53 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+  rw [le_abs]
+  left
+  apply le_refl
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+  rw [le_abs]
+  right
+  apply le_refl
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rw [abs_le]
+  constructor
+  · rw [neg_add]
+    apply add_le_add <;> apply neg_abs_le
+  · exact add_le_add (le_abs_self x) (le_abs_self y)
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+  rw [abs]
+  constructor
+  · intro h
+    rcases max_choice y (-y) with pos | neg
+    · left
+      rw [pos] at h
+      exact h
+    · rw [neg] at h
+      exact Or.inr h
+
+  · rintro (lt | ltn)
+    · exact lt_max_of_lt_left lt
+    · exact lt_max_of_lt_right ltn
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  obtain ⟨xeq, _hx⟩ | ⟨xeq, _hx⟩ := abs_cases x
+  <;> rw [xeq]
+  <;> exact ⟨fun _ => ⟨by linarith, by linarith⟩, fun _ => by linarith⟩
+
+  -- obtain ⟨xeq, hx⟩ | ⟨xeq, hx⟩ := abs_cases x
+  -- · rw [xeq]
+  --   constructor
+  --   · intro h; exact ⟨by linarith, h⟩
+  --   -- · exact fun ⟨_, lt⟩ ↦ lt
+  --   · exact fun _ ↦ by linarith
+
+  -- · rw [xeq]
+  --   constructor
+  --   · intros; exact ⟨by linarith, by linarith⟩
+  --   · exact fun _ => by linarith
 
 end MyAbs
 
@@ -91,23 +125,32 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  obtain ⟨x, y, h | h⟩ := h <;> rw [h]
+  · apply add_nonneg <;> apply pow_two_nonneg
+  · apply add_nonneg
+    apply add_nonneg <;> apply pow_two_nonneg
+    linarith
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  rw [pow_two] at h
+  exact mul_self_eq_one_iff.mp h
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  rw [pow_two, pow_two] at h
+  exact mul_self_eq_mul_self_iff.mp h
+
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  rw [pow_two] at h
+  exact mul_self_eq_one_iff.mp h
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  rw [pow_two, pow_two] at h
+  exact mul_self_eq_mul_self_iff.mp h
 
 end
 
@@ -124,5 +167,11 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
-
+  constructor
+  · intro h
+    by_cases h' : P
+    exact Or.inr (h h' : Q)
+    exact Or.inl (h' : ¬P)
+  · rintro (np | q) h
+    exact False.elim (np h)
+    exact q
