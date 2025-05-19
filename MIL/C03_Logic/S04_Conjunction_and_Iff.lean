561fd8d2f756b9f86ca51b7d78309306a3c8baec
autf <autf@dev.null>
2025-05-19 17:12:46 +0800

    c3s4 (by tac : prop) (f x y : prop)

--- MIL/C03_Logic/S04_Conjunction_and_Iff.lean
+++ MIL/C03_Logic/S04_Conjunction_and_Iff.lean
@@ -63,8 +63,12 @@ example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x := by
 example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x :=
   fun h' ↦ h.right (le_antisymm h.left h')
 
-example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) : m ∣ n ∧ ¬n ∣ m :=
-  sorry
+example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) : m ∣ n ∧ ¬n ∣ m := by
+  rcases h with ⟨h', mne⟩
+  constructor
+  · exact h'
+  · intro h
+    exact mne (dvd_antisymm h' h)
 
 example : ∃ x : ℝ, 2 < x ∧ x < 4 :=
   ⟨5 / 2, by norm_num, by norm_num⟩
@@ -74,7 +78,7 @@ example (x y : ℝ) : (∃ z : ℝ, x < z ∧ z < y) → x < y := by
   exact lt_trans xltz zlty
 
 example (x y : ℝ) : (∃ z : ℝ, x < z ∧ z < y) → x < y :=
-  fun ⟨z, xltz, zlty⟩ ↦ lt_trans xltz zlty
+  fun ⟨_z, xltz, zlty⟩ ↦ lt_trans xltz zlty
 
 example : ∃ x : ℝ, 2 < x ∧ x < 4 := by
   use 5 / 2
@@ -101,15 +105,26 @@ example {x y : ℝ} (h : x ≤ y) : ¬y ≤ x ↔ x ≠ y := by
 example {x y : ℝ} (h : x ≤ y) : ¬y ≤ x ↔ x ≠ y :=
   ⟨fun h₀ h₁ ↦ h₀ (by rw [h₁]), fun h₀ h₁ ↦ h₀ (le_antisymm h h₁)⟩
 
-example {x y : ℝ} : x ≤ y ∧ ¬y ≤ x ↔ x ≤ y ∧ x ≠ y :=
-  sorry
+example {x y : ℝ} : x ≤ y ∧ ¬y ≤ x ↔ x ≤ y ∧ x ≠ y := by
+  constructor
+  · rintro ⟨xle, ynle⟩
+    constructor
+    exact xle
+    apply ne_of_lt
+    exact lt_of_not_ge ynle
+
+  · rintro ⟨xle, xne⟩
+    exact ⟨xle, lt_iff_not_le.mp (lt_of_le_of_ne xle xne)⟩
 
 theorem aux {x y : ℝ} (h : x ^ 2 + y ^ 2 = 0) : x = 0 :=
-  have h' : x ^ 2 = 0 := by sorry
+  have h' : x ^ 2 = 0 := eq_zero_of_add_nonpos_left (sq_nonneg x) (sq_nonneg y) (le_of_eq h)
   pow_eq_zero h'
 
 example (x y : ℝ) : x ^ 2 + y ^ 2 = 0 ↔ x = 0 ∧ y = 0 :=
-  sorry
+  ⟨
+    fun h ↦ ⟨aux h, pow_eq_zero (eq_zero_of_add_nonpos_right (sq_nonneg x) (sq_nonneg y) (le_of_eq h))⟩,
+    fun ⟨x0, y0⟩ ↦ by rw [x0, y0, zero_pow (by trivial : 2 ≠ 0), zero_add]
+  ⟩
 
 section
 
@@ -130,7 +145,16 @@ theorem not_monotone_iff {f : ℝ → ℝ} : ¬Monotone f ↔ ∃ x y, x ≤ y �
   rfl
 
 example : ¬Monotone fun x : ℝ ↦ -x := by
-  sorry
+  -- dsimp only [Monotone]
+  rw [Monotone]
+  push_neg
+  use 0, 1
+  exact ⟨by norm_num, by norm_num⟩
+  -- intro h
+  -- have this := @h 0 1 (by norm_num)
+  -- dsimp at this
+  -- have : ¬-(0 : ℝ) ≤ -1 := by norm_num
+  -- linarith
 
 section
 variable {α : Type*} [PartialOrder α]
@@ -138,7 +162,22 @@ variable (a b : α)
 
 example : a < b ↔ a ≤ b ∧ a ≠ b := by
   rw [lt_iff_le_not_le]
-  sorry
+  constructor
+  · rintro ⟨aleb, bnlea⟩
+    if h : a = b then
+      exfalso
+      apply bnlea
+      rw [h]
+    else
+      rw [ne_eq]
+      exact ⟨aleb, h⟩
+
+  · rintro ⟨aleb, aneb⟩
+    constructor
+    · exact aleb
+    · intro blea
+      apply aneb
+      exact le_antisymm aleb blea
 
 end
 
@@ -148,10 +187,15 @@ variable (a b c : α)
 
 example : ¬a < a := by
   rw [lt_iff_le_not_le]
-  sorry
+  rintro ⟨h, nh⟩
+  exact nh h
 
 example : a < b → b < c → a < c := by
   simp only [lt_iff_le_not_le]
-  sorry
+  rintro ⟨aleb, bnlea⟩ ⟨blec, _cnleb⟩
+  constructor
+  · exact le_trans aleb blec
+  · by_contra clea
+    exact bnlea (le_trans blec clea : b ≤ a)
 
 end
